b1d90fcf764089cf866c7726e850baa7602a7c62
KKiumbe <97431990+KKiumbe@users.noreply.github.com>
2025-04-29 17:32:07 +0300

    prod ready user activity

--- controller/auth/signupSignIn.js
+++ controller/auth/signupSignIn.js
@@ -229,8 +229,9 @@ const signin = async (req, res) => {
       // Log the login action in userActivity
       await tx.userActivity.create({
         data: {
-          userId: user.id,
-          tenantId: user.tenantId,
+          user: { connect: { id: user.id } },
+          tenant: { connect: { id: user.tenantId } },
+          //tenantId: user.tenantId,
           action: ACTION_TYPES.LOGIN,
           details: { message: 'User logged in successfully' }, // Optional: Add context
         },
