255749e33f5e869d1791853ca4cda558506e02f5
KKiumbe <97431990+KKiumbe@users.noreply.github.com>
2025-07-11 17:23:38 +0300

    more_payment_check_for_tenant

--- routes/payment/paymentRoutes.js
+++ routes/payment/paymentRoutes.js
@@ -2,19 +2,21 @@ const express = require('express');
 const { fetchAllPayments, fetchPaymentById, fetchPaymentsByTransactionId, getAllPayments, searchPaymentsByName, searchPaymentsByPhone, getUnreceiptedPayments, searchTransactionById, filterPaymentsByMode } = require('../../controller/payments/getAllPayments.js');
 const verifyToken = require('../../middleware/verifyToken.js');
 const checkAccess = require('../../middleware/roleVerify.js');
+const { checkTenantStatus, requireTenantStatus } = require('../../middleware/requireTenantStatus.js');
+const { TenantStatus } = require('@prisma/client');
 
 const router = express.Router();
 
 
-router.get('/payments',verifyToken,checkAccess('payments','read'), getAllPayments);
-router.get('/payments/unreceipted',verifyToken,checkAccess('payments','read'), getUnreceiptedPayments);
-router.get('/payments/search-by-name',verifyToken,checkAccess('payments','read'), searchPaymentsByName);
-router.get('/payments/search-by-phone',verifyToken,checkAccess('payments','read'), searchPaymentsByPhone);
+router.get('/payments',verifyToken, checkTenantStatus,requireTenantStatus([TenantStatus.ACTIVE]), checkAccess('payments','read'), getAllPayments);
+router.get('/payments/unreceipted',verifyToken,checkTenantStatus,requireTenantStatus([TenantStatus.ACTIVE]),checkAccess('payments','read'), getUnreceiptedPayments);
+router.get('/payments/search-by-name',verifyToken,checkTenantStatus,requireTenantStatus([TenantStatus.ACTIVE]), checkAccess('payments','read'), searchPaymentsByName);
+router.get('/payments/search-by-phone',verifyToken, checkTenantStatus,requireTenantStatus([TenantStatus.ACTIVE]),checkAccess('payments','read'), searchPaymentsByPhone);
 
-router.get('/searchTransactionById', verifyToken,checkAccess('payments','read'), searchTransactionById);
-router.get('/filterPaymentsByMode',verifyToken,checkAccess('payments','read'), filterPaymentsByMode);
-router.get('/payments/:paymentId', verifyToken,checkAccess('payments','read'), fetchPaymentById);
-router.get('/payments-search', verifyToken, checkAccess('payments','read'), fetchPaymentsByTransactionId);
+router.get('/searchTransactionById', verifyToken,checkTenantStatus,requireTenantStatus([TenantStatus.ACTIVE]),checkAccess('payments','read'), searchTransactionById);
+router.get('/filterPaymentsByMode',verifyToken,checkTenantStatus,requireTenantStatus([TenantStatus.ACTIVE]),checkAccess('payments','read'), filterPaymentsByMode);
+router.get('/payments/:paymentId', verifyToken,checkTenantStatus,requireTenantStatus([TenantStatus.ACTIVE]),checkAccess('payments','read'), fetchPaymentById);
+router.get('/payments-search', verifyToken,checkTenantStatus,requireTenantStatus([TenantStatus.ACTIVE]), checkAccess('payments','read'), fetchPaymentsByTransactionId);
 
 
 module.exports = router;
